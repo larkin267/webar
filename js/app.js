04a34e8bc0d47e38af16ea5148c40785a6609781
larkin <49519523+larkin267@users.noreply.github.com>
2021-12-16 14:36:40 +0800

    build1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-build3.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-build.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
