e6ab80749c0ccd2baa5d04de24ed86cc55fad002
larkin <49519523+larkin267@users.noreply.github.com>
2021-12-16 14:27:28 +0800

    markerless-singleimage

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-build2.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
