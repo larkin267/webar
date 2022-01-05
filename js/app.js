4acb28c7e55b39398d8e60488272414f4e15155c
larkin <49519523+larkin267@users.noreply.github.com>
2022-01-05 22:04:51 +0800

    rhino

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-mybuild-intro.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT-mybuild-model.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
