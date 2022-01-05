e0a493cd3a6b390fbc55407a5cc492c5123b9c7f
larkin <49519523+larkin267@users.noreply.github.com>
2022-01-05 23:09:01 +0800

    new

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/model3.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/model4.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
